4275d7a82de3c8f6dc619340f283efca98f397f6
Mitchell Campbell Torstonson <116796839+syk01N3R@users.noreply.github.com>
2024-05-10 10:55:04 +1200

    header

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -5,7 +5,8 @@ import Header from './Header.tsx'
 function App() {
   return (
     <div>
-      <h1>App 'Gabby is that you!?'</h1>
+      <Header/>
+      <h1>App Gabby is that you!</h1>
       <p>React development has begun!</p>
       
       <div className="placeHolder">
---
+++ client/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return <header className="header">Gabby is that you?@! Sound Board{/* Content of the header will go here */}
+    
+    </header>
+
+    
+  }
+  
+
+
+  export default Header 
