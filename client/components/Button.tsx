124afdd5155aa3856f3bb269944d5a26a1987184
David Bowers-Mason <davidowersmason@gmail.com>
2024-05-09 20:44:01 +1200

    add divs and spans to button linked to current css

---
+++ client/components/Button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+    effectName: string
+}
+
+function Button(props: Props) {
+    return (
+        <div className="button-wrapper">
+            <div className="button">
+                <span className="sound-name">
+                    "sound names yet to be configured"
+                </span>
+            </div>
+        </div>
+    )
+}
