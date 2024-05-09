e801a2365ee6a5657bde7de7ca49ad05ac4e9dd8
Corina Reitemeyer <corina@corina.org>
2024-05-10 09:39:48 +1200

    Adjust sound library to be an object array

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -1,36 +1,26 @@
 import { useState } from 'react'
+import data from './sounds.ts'
 
 interface Props {
   effectName: string
 }
 
 function Button(props: Props) {
-  const [currentSound, setSound] = useState(false)
-
-  const soundFiles = [
-    // Will move those to a seperate file soon!
-    'ba-dum-tss.wav',
-    'carnival.wav',
-    'country-vibes.wav',
-    'fbi-open-up.mp3',
-    'its-an-avocado-thanks.mp3',
-    'look-at-this-distinguished-gentleman-meme.wav',
-    'ratatouille-oh-my-god-meme-sound.mp3',
-    'something-tells-me-thats-not-funny.mp3',
-    'taste-like-chicken-remix.mp3',
-    'your-worst-night-mare.mp3',
-  ]
+  const [currentSound, setSound] = useState(null)
 
   const handleClick = (soundFile: string) => {
     const audio = new Audio(soundFile) // Creates a new HTMLAudioelement object
-    audio.play()
-    setSound(true)
+    audio.play().catch((error) => console.error('Error playing audio:', error))
+    setSound(soundFile)
   }
   return (
     <div className="button-wrapper">
-      {soundFiles.map((soundFile, index) => (
+      {data.map((soundFile, index) => (
         <button key={index} onClick={() => handleClick(soundFile)}>
-          <span className="sound-name">{props.effectName}</span>
+          <span className="sound-name">
+            {props.effectName}
+            {index + 1}
+          </span>
         </button>
       ))}
       {currentSound && <p> Currently playing: {currentSound} </p>}
