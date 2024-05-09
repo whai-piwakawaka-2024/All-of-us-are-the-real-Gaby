0870863aaf86f58819098750ac0356139e039840
David Bowers-Mason <davidowersmason@gmail.com>
2024-05-09 21:55:56 +1200

    commit for the main branch

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -5,13 +5,17 @@ import Header from './Header.tsx'
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <h1>App 'Gabby is that you!?'</h1>
       <p>React development has begun!</p>
-      <Button effectName="bababooey"/>
-      <Button effectName="kill me" />
-      <Button effectName="I am the real Gaby" />
+      
+      <div className="placeHolder">
+        <Button effectName="bababooey"/>
+        <Button effectName="kill me" />
+        <Button effectName="I am the real Gaby" />
+      </div>
     </div>
   )
 }
 
 export default App
+
