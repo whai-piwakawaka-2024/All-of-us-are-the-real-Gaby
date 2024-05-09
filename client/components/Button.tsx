f50c71d2a799caad31c51199ec5cd682aa82bd9b
Corina Reitemeyer <corina@corina.org>
2024-05-09 23:18:56 +1200

    Add array of sound files

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -7,6 +7,20 @@ interface Props {
 function Button(props: Props) {
   const [sound, setSound] = useState(false)
 
+  const soundFiles = [
+    // Will move those to a seperate file soon!
+    'ba-dum-tss.wav',
+    'carnival.wav',
+    'country-vibes.wav',
+    'fbi-open-up.mp3',
+    'its-an-avocado-thanks.mp3',
+    'look-at-this-distinguished-gentleman-meme.wav',
+    'ratatouille-oh-my-god-meme-sound.mp3',
+    'something-tells-me-thats-not-funny.mp3',
+    'taste-like-chicken-remix.mp3',
+    'your-worst-night-mare.mp3',
+  ]
+
   const handleClick = () => {
     const audio = new Audio('import audio here') // Creates a new HTMLAudioelement object
     audio.play()
