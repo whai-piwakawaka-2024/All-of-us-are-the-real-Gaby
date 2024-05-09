9695fd2f9d4f038849ce50d35a022fb8ff867212
David Bowers-Mason <davidowersmason@gmail.com>
2024-05-09 20:48:55 +1200

    call Button.tsx in App.tsx to have a working example

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,15 @@
+import Button from './Button.tsx'
+import Header from './Header.tsx'
+
+
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+      <Button effectName="bababooey"/>
+      <Button effectName="kill me" />
+      <Button effectName="I am the real Gaby" />
     </div>
   )
 }
