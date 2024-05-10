053fc9b682f399ea7d6dbd7a98b4e0045ab39f2d
Mitchell Campbell Torstonson <116796839+syk01N3R@users.noreply.github.com>
2024-05-10 14:11:04 +1200

    bears

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,11 +1,19 @@
 function Header() {
-    return <header className="header">Gabby is that you?@! Sound Board{/* Content of the header will go here */}
-    
+  return (
+    <header className="header">
+      <img
+        className="reactiveImg"
+        src="https://images.pexels.com/photos/35435/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="Gaby is that you?!"
+      />
+      <h2>@Gaby is that you?! Sound Board</h2>
+      <img
+        className="mirroredImg"
+        src="https://images.pexels.com/photos/35435/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="Gaby is that you?!"
+      />
     </header>
+  )
+}
 
-    
-  }
-  
-
-
-  export default Header 
+export default Header
