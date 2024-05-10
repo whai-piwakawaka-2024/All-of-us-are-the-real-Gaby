b1ba5b2b70ee784c01310c9c7ceaf0afba24a007
Corina Reitemeyer <corina@corina.org>
2024-05-10 14:35:30 +1200

    Set sound to buttons and prevent overlapping

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import data from './data/sounds'
 
 function Button() {
-  const [currentSound, setSound] = useState(null || 'string')
+  const [currentSound, setSound] = useState(null as HTMLAudioElement)
 
-  const handleClick = (soundFile: soundFiles) => {
+  const handleClick = (soundFile: string) => {
     if (typeof soundFile === 'object' && soundFile.name) {
-      playAudio(soundFile)
+      playAudio(soundFile.name)
     } else {
       console.error('Invalid sound file:', soundFile)
     }
@@ -19,12 +19,17 @@ function Button() {
     }
 
     const findAudio = data.find((item) => item.name === name)
+    console.log(findAudio)
     if (findAudio) {
       const audioPath: string = findAudio.src
       console.log('Audio file:', audioPath)
       const audio = new Audio(audioPath)
+      if (currentSound) {
+        currentSound.pause()
+        currentSound.currentTime = 0
+      }
+      setSound(audio)
       audio.play()
-      setSound(audioPath)
     } else {
       console.error('Audio file not found')
     }
--- client/components/data/sounds.ts
+++ client/components/data/sounds.ts
@@ -7,39 +7,39 @@ interface soundFiles {
 export type soundFilesData = Record<string, soundFiles>
 
 const data = [
-  { id: 1, name: 'Ba dum tss!', src: '../../sound-files/ba-dum-tss.wav' },
-  { id: 2, name: 'Carnival', src: '../../sound-files/carnival.wav' },
-  { id: 3, name: 'Country vibe', src: '../../sound-files/country-vibes.wav' },
-  { id: 4, name: 'FBI open up', src: '../../sound-files/fbi-open-up.mp3' },
+  { id: 1, name: 'Ba dum tss!', src: '/sound-files/ba-dum-tss.wav' },
+  { id: 2, name: 'Carnival', src: '/sound-files/carnival.wav' },
+  { id: 3, name: 'Country vibe', src: '/sound-files/country-vibes.wav' },
+  { id: 4, name: 'FBI open up', src: '/sound-files/fbi-open-up.mp3' },
   {
     id: 5,
     name: 'It is an avocado thanks',
-    src: '../../sound-files/its-an-avocado-thanks.mp3',
+    src: '/sound-files/its-an-avocado-thanks.mp3',
   },
   {
     id: 6,
     name: 'Look at this distinguished gentleman',
-    src: '../../sound-files/look-at-this-distinguished-gentleman-meme.wav',
+    src: '/sound-files/look-at-this-distinguished-gentleman-meme.wav',
   },
   {
     id: 7,
     name: 'Oh my god',
-    src: '../../sound-files/ratatouille-oh-my-god-meme-sound.mp3',
+    src: '/sound-files/ratatouille-oh-my-god-meme-sound.mp3',
   },
   {
     id: 8,
     name: 'Something tells me that is not funny',
-    src: '../../sound-files/something-tells-me-thats-not-funny.mp3',
+    src: '/sound-files/something-tells-me-thats-not-funny.mp3',
   },
   {
     id: 9,
     name: 'Taste like chicken remix',
-    src: '../../sound-files/taste-like-chicken-remix.mp3',
+    src: '/sound-files/taste-like-chicken-remix.mp3',
   },
   {
     id: 10,
     name: 'Your worst nightmare',
-    src: '../../sound-files/your-worst-night-mare.mp3',
+    src: '/sound-files/your-worst-night-mare.mp3',
   },
 ]
 
